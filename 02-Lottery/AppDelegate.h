2131c1a18f9d43d12b936821d205109816c966a5
jiongjiongwang <wjsnh@aliyun.com>
2016-05-27 11:53:47 +0800

    Initial Commit

---
+++ 02-Lottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02-Lottery
+//
+//  Created by 王炯 on 16/5/27.
+//  Copyright © 2016年 王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02-Lottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02-Lottery
+//
+//  Created by 王炯 on 16/5/27.
+//  Copyright © 2016年 王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
